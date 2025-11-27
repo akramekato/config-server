ef68b8b4052163f475b41ab4a642bbd45f4482f6
kato <akramkato72@gmail.com>
2025-11-27 17:46:30 +0100

    test

--- src/main/java/ma/gov/pfe/configserver/ConfigServerApplication.java
+++ src/main/java/ma/gov/pfe/configserver/ConfigServerApplication.java
@@ -1,4 +1,16 @@
 package ma.gov.pfe.configserver;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@Slf4j
 public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
